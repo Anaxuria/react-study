15d4201993c6edfd4167fa40e38f9b27732306d7
anaxuria <anajuliagarcia.ajg@gmail.com>
2023-09-04 16:30:01 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Post} from './Post'
+
+export function App() {
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Post author="Anaxuria" content="Lorem, ipsum dol sit amet consectetur adipisicing elit. Fuga alias deserunt, nisi vitae perspiciatis doloribus, dolores vero excepturi"/>
+      <Post author="Xuriaana" content="Fuga alias deserunt, nisi vitae perspiciatis doloribus, dolores vero excepturi"/>
+    </div>
+  )
+}
+
